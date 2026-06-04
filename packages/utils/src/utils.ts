34140a5abd4dc003d40d5bda5ca00f98e372b36d
vyar20 <56675665+vyar20@users.noreply.github.com>
2026-06-04 20:16:45 +0700

    Add HTTP status enums and utility function
    Added HTTP_STATUS and HTTP_STATUS_TEXT enums along with a getHTTPStatus function to retrieve status codes based on status text.

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -1,4 +1,4 @@
-import { ClassValue, clsx } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export class ErrorHandler extends Error {
@@ -12,6 +12,32 @@ export class ErrorHandler extends Error {
     }
 }
 
-export const p = <T> (p: Promise<T>): Promise<[null, T] | [Error | ErrorHandler]> => p.then(result => [null, result] as [null, T]).catch(error => [error])
+export const p = <T>(p: Promise<T>): Promise<[null, T] | [Error | ErrorHandler]> =>
+    p.then((result) => [null, result] as [null, T]).catch((error) => [error])
 
 export const cn = (...classes: ClassValue[]) => twMerge(clsx(classes))
+
+export enum HTTP_STATUS {
+    OK = 200,
+    CREATED = 201,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404
+}
+
+export enum HTTP_STATUS_TEXT {
+    OK = "OK",
+    CREATED = "CREATED",
+    BAD_REQUEST = "BAD_REQUEST",
+    UNAUTHORIZED = "UNAUTHORIZED",
+    FORBIDDEN = "FORBIDDEN",
+    NOT_FOUND = "NOT_FOUND"
+}
+
+export const getHTTPStatus = (statusText: keyof typeof HTTP_STATUS_TEXT) => {
+    const [_, statusCode] = Object.entries(HTTP_STATUS).find(([key]) => key === statusText) || []
+    return statusCode
+}
+
+getHTTPStatus("OK")
