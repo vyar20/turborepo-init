4a5b7c5c276bafcdc8ee59d20187f5c9c7c7c451
vyar20 <56675665+vyar20@users.noreply.github.com>
2026-06-09 13:24:11 +0700

    Update prettier.js

--- packages/config/prettier.js
+++ packages/config/prettier.js
@@ -5,7 +5,7 @@ module.exports = {
   printWidth: 100,
   tabWidth: 4,
   semi: false,
-  singleQuote: false,
+  singleQuote: true,
   jsxSingleQuote: true,
   trailingComma: 'none',
   plugins: ['../../packages/config/node_modules/prettier-plugin-tailwindcss/dist/index.mjs']
